906ea02296b4ec5d7b42953540780fe96bf4f688
gothica <semidevilish666@gmail.com>
2023-01-03 20:56:48 +0300

    fix: derloy router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <HashRouter basename="/">
-                <App />
-            </HashRouter>
-        </BrowserRouter>
+        <HashRouter basename="/">
+            <App />
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
